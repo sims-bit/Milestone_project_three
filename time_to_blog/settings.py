b6b43aa2be999889cdc8b6ecea4e63195710fb17
sims-bit <xfiqoe46@students.codeinstitute.net>
2025-05-02 17:51:32 +0100

    Create Post Model

--- time_to_blog/settings.py
+++ time_to_blog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'time_to_blog.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
